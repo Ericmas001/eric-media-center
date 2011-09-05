e4be0bd6d6fe26fa3f426ce7282e926be44806be
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2011-09-05 20:28:03 +0000

    Auto Update of parser is working :)

--- EricMediaCenter/EricMediaCenter/Form1.cs
+++ EricMediaCenter/EricMediaCenter/Form1.cs
@@ -9,12 +9,16 @@ using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
 using EMCMasterPluginLib;
+using EricUtility.Networking.Gathering;
+using EricUtilityNetworking;
 
 namespace EricMediaCenter
 {
     public partial class Form1 : Form
     {
         IEMCParserPlugin parser = null;
+
+        string next = null;
 
         /// <summary>
         ///   Gets the fully qualified name of the ".options.xml" file.
@@ -32,7 +36,7 @@ namespace EricMediaCenter
         ///    "C:\Documents and Settings\<i>username</i>\Application Data\<i>company</i>\<i>product</i>\<i>appname</i>.exe.options.xml".
         ///    </para>
         /// </remarks>
-        private static string ParserPath
+        private static string EMCPath
         {
             get
             {
@@ -47,6 +51,16 @@ namespace EricMediaCenter
                     if (!Directory.Exists(dir))
                         Directory.CreateDirectory(dir);
                 }
+                return path.ToString();
+            }
+        }
+        private static string ParserPath
+        {
+            get
+            {
+                // Build the directory.
+                StringBuilder path = new StringBuilder();
+                path.Append(EMCPath);
 
                 // Add the file name.
                 path.Append(Path.DirectorySeparatorChar);
@@ -77,8 +91,58 @@ namespace EricMediaCenter
                         label1.Text = "Parser loaded, version " + parser.Version;
                     }
                 }
-                //StreamReader sr = File.OpenText("http://www.ericmas001.com/EMC/plugins/list.txt");
-                //string list = sr.ReadToEnd();
+            }
+            else
+                label1.Text = "No Parser found";
+            
+                string list = GatheringUtility.GetPageSource("http://www.ericmas001.com/EMC/plugins/list.txt");
+                string[] plugins = list.Split('\n');
+                foreach (string p in plugins)
+                {
+                    string[] info = p.Split(';');
+                    string name = info[0];
+                    Version v = new Version(info[1]);
+                    if( name == "parser" )
+                    {
+                        if (parser == null || v > parser.Version)
+                        {
+                            label1.Text = "A new version of the parser is available !!!";
+                            next = "http://www.ericmas001.com/EMC/plugins/parser_" + v.ToString(3) + ".dll";
+                            button1.Visible = true;
+                        }
+                        else
+                            button1.Visible = false;
+                    }
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DownloadItem it = new DownloadItem(next, EMCPath, "EMCParser.dll");
+            button1.Visible = false;
+            it.DownloadFileCompleted += new AsyncCompletedEventHandler(it_DownloadFileCompleted);
+            it.StartDownload();
+        }
+
+        void it_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            label1.Text = "Téléchargement terminé";
+
+            if (File.Exists(ParserPath))
+            {
+                Assembly ass = Assembly.LoadFile(ParserPath);
+                Type[] types = ass.GetTypes();
+                bool found = false;
+                foreach (Type t in ass.GetTypes())
+                {
+                    if (!found && t.GetInterface("IEMCParserPlugin") != null)
+                    {
+                        found = true;
+                        parser = (IEMCParserPlugin)Activator.CreateInstance(t);
+                        listBox1.Items.AddRange(parser.GetSupportedWebsites());
+                        label1.Text = "Parser loaded, version " + parser.Version;
+                    }
+                }
             }
             else
                 label1.Text = "No Parser found";
