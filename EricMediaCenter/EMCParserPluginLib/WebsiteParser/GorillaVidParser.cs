b90d6461e6f51dd786b97f7a9b237929077c0d0b
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-01-11 22:53:54 +0000

    0.2.5

---
+++ EricMediaCenter/EMCParserPluginLib/WebsiteParser/GorillaVidParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMCMasterPluginLib;
+using EricUtility;
+using EricUtility.Networking.Gathering;
+
+namespace EMCParserPluginLib.WebsiteParser
+{
+    public class GorillaVidParser : IWebsiteParser
+    {
+        public ParsedWebsite FindInterestingContent(string res, string url, System.Net.CookieContainer cookies)
+        {
+            while (res.Contains("Please wait while we verify your request"))
+            {
+                string id = StringUtility.Extract(res, "<input type=\"hidden\" name=\"id\" value=\"", "\">");
+                string fname = StringUtility.Extract(res, "<input type=\"hidden\" name=\"fname\" value=\"", "\">");
+                string post = "op=download1&usr_login=&id=" + id + "&fname=" + fname + "&referer=&channel=cna&method_free=tel%3Fchargement+libre+";
+                res = GatheringUtility.GetPageSource("http://gorillavid.com/cna/" + id, cookies, "op=download1&usr_login=&id=" + id + "&fname=" + fname + "&referer=&channel=cna&method_free=tel%3Fchargement+libre+");
+            }
+
+            string newurl = StringUtility.Extract(res, "file:\"", "\"");
+            if (newurl != null)
+                return new ParsedWebsite(url, ParsedWebsite.Extension.Flv, newurl);
+            else
+                return new ParsedWebsite(url);
+        }
+    }
+}
---
+++ EricMediaCenter/EMCParserPluginLib/WebsiteParser/PutLockerSockShareParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMCMasterPluginLib;
+using EricUtility;
+using EricUtility.Networking.Gathering;
+
+namespace EMCParserPluginLib.WebsiteParser
+{
+    public class PutLockerSockShareParser : IWebsiteParser
+    {
+        public ParsedWebsite FindInterestingContent(string res, string url, System.Net.CookieContainer cookies)
+        {
+            string beginurl = "http://www.sockshare.com";
+            if( url.Contains("www.putlocker.com") )
+                beginurl = "http://www.putlocker.com";
+           
+            while (res.Contains("Continue as Free User"))
+            {
+                string u = GatheringUtility.GetPageUrl(url, cookies, "", "application/x-www-form-urlencoded");
+                url = beginurl + u;
+                string hash = StringUtility.Extract(res, "<input type=\"hidden\" value=\"", "\"");
+                res = GatheringUtility.GetPageSource(url, cookies, "hash=" + hash + "&confirm=Continue+as+Free+User");
+            }
+            if (res.Contains("This file doesn't exist"))
+                return new ParsedWebsite(url);
+            string rssU = beginurl + StringUtility.Extract(res, "playlist: '", "',");
+            string info = GatheringUtility.GetPageSource(rssU, cookies);
+            string result = StringUtility.Extract(info, "<media:content url=\"", "\"");
+
+            return new ParsedWebsite(url, ParsedWebsite.Extension.Flv, result);
+        }
+    }
+}
