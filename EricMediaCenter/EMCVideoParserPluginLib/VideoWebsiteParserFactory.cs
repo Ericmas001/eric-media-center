aff92b552b43fd23846c709de0a6385e99f4f3cf
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-01-12 00:36:41 +0000

    0.2.6

--- EricMediaCenter/EMCVideoParserPluginLib/VideoWebsiteParserFactory.cs
+++ EricMediaCenter/EMCVideoParserPluginLib/VideoWebsiteParserFactory.cs
@@ -11,19 +11,25 @@ namespace EMCVideoParserPluginLib
     {
         public Version Version
         {
-            get { return new Version(0, 2, 5); }
+            get { return new Version(0, 2, 6); }
         }
         private static readonly Dictionary<string, IVideoWebsiteParser> Supported = new Dictionary<string, IVideoWebsiteParser>()
         {
-            //{"megavideo.com",new MegaVideoParser()}, BROKEN
-            //{"novamov.com",new NovaMovParser()}, BROKEN
-
-            //{"playhd.org",new PlayHDParser()}, GONE :(
+            //{"megavideo.com",new MegaVideoParser()},  BROKEN :(
+            //{"novamov.com",new NovaMovParser()},      BROKEN :(
+            //{"videoweed.com",new VideoWeedParser()},  BROKEN :(
+            //{"videoweed.es",new VideoWeedParser()},   BROKEN :(
+            //{"youtube.com",new YoutubeParser()},      BROKEN :(
+            //{"zshare.net",new ZShareParser()},        BROKEN :(
+            
+            //{"playhd.org",new PlayHDParser()},    GONE :(
+            //{"wisevid.com",new WiseVidParser()},  GONE :(
             
             {"gorillavid.com",new GorillaVidParser()},
             {"movshare.net",new MovShareParser()},
             {"putlocker.com",new PutLockerSockShareParser()},
             {"sockshare.com",new PutLockerSockShareParser()},
+            {"stagevu.com",new StageVuParser()},
         };
 
         public string UniqueName
