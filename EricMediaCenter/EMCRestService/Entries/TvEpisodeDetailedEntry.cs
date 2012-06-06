43d00df82b5f14ca9b5a2d0a909a22f543281ff2
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-06-06 02:51:05 +0000

    WatchSeries.GetEpisode !

---
+++ EricMediaCenter/EMCRestService/Entries/TvEpisodeDetailedEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMCRestService.Entries
+{
+    public class TvEpisodeDetailedEntry : TvEpisodeEntry
+    {
+        private int m_SeasonNo;
+
+        public int SeasonNo
+        {
+            get { return m_SeasonNo; }
+            set { m_SeasonNo = value; }
+        }
+        private string m_Description;
+
+        public string Description
+        {
+            get { return m_Description; }
+            set { m_Description = value; }
+        }
+        private string m_ShowTitle;
+
+        public string ShowTitle
+        {
+            get { return m_ShowTitle; }
+            set { m_ShowTitle = value; }
+        }
+        private Dictionary<string, List<int>> m_Links;
+
+        public Dictionary<string, List<int>> Links
+        {
+            get { return m_Links; }
+            set { m_Links = value; }
+        }
+    }
+}
