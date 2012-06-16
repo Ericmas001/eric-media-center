103c7bb17d6910cdab86a3696692a3dd65e2b73b
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-06-16 02:49:02 +0000

    WatchSeriesAppPlugin

---
+++ trunk/EricMediaCenter/WatchSeriesAppPlugin/WatchSeriesPlugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMCMasterPluginLib.Application;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WatchSeriesAppPlugin
+{
+    public class WatchSeriesPlugin : IEMCApplicationPlugin
+    {
+        #region IEMCApplicationPlugin Members
+
+        public IApplication GetApplication()
+        {
+            return new WatchSeriesApp();
+        }
+
+        #endregion
+
+        #region IEMCMasterPlugin Members
+
+        public string UniqueName
+        {
+            get { return "WatchSeriesApp"; }
+        }
+
+        public string FullName
+        {
+            get { return "Application WatchSeries"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(0, 0, 1); }
+        }
+
+        public bool Load()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
