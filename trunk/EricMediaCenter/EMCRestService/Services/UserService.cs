f9795eb3f6d360fa140145ec6d087fa9747207de
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-06-16 01:30:47 +0000

    UIserService refactored + now returning a sessionID

--- trunk/EricMediaCenter/EMCRestService/Services/UserService.cs
+++ trunk/EricMediaCenter/EMCRestService/Services/UserService.cs
@@ -1,63 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
-using System.Text;
-using Newtonsoft.Json;
-using EricUtility.Networking.Gathering;
 using EricUtility;
-using EricUtility2011;
-using System.Globalization;
-using System.Data.SqlClient;
+using EricUtility2011.Data;
+using Newtonsoft.Json;
 
 namespace EMCRestService.Services
 {
-    // user emc.webservice ... pass Emc42FTW
     [ServiceContract]
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class UserService
     {
+        private SqlServerConnector Connector = new SqlServerConnector("TURNSOL.arvixe.com", "emc", "emc.webservice", "Emc42FTW");
+
         [WebGet(UriTemplate = "Connect/{user}/{pass}")]
         public string Connect(string user, string pass)
         {
+            string error = null;
             bool ok = false;
-
-            SqlConnection myConnection = new SqlConnection("user id=emc.webservice;" +
-                                       "password=Emc42FTW;server=TURNSOL.arvixe.com;" +
-                                       "Trusted_Connection=no;" +
-                                       "database=emc; " +
-                                       "connection timeout=30");
+            SqlConnection myConnection = Connector.GetConnection();
             try
             {
-                myConnection.Open();
+                Dictionary<string, object> result = Connector.SelectOneRow(myConnection, "select * from ericmas001.TUser where username = @User", new Dictionary<string, object>() { { "User", user } });
+                if (pass.Trim() == result["password"].ToString().Trim())
+                    ok = true;
+                if (!ok)
+                    error = "User or Password is incorrect";
             }
             catch (Exception e)
             {
-                return "Connection Error: " + e.ToString();
+                error = "Read Error: " + e.ToString();
             }
 
-
-            try
+            if (ok)
             {
-                SqlDataReader myReader = null;
-                SqlCommand myCommand = new SqlCommand("select * from ericmas001.TUser where username = '" + user + "'",
-                                                         myConnection);
-                myReader = myCommand.ExecuteReader();
-                if (myReader.Read())
-                    if (pass.Trim() == myReader["password"].ToString().Trim())
-                        ok = true;
+                string t = StringUtility.GetMd5Hash(user + ";" + DateTime.Now.ToString());
+                DateTime validUntil = DateTime.Now + TimeSpan.FromMinutes(5);
+                Connector.Execute(myConnection, "update ericmas001.TUser set lastToken = @Token, tokenValidUntil = @Valid where username = @User", new Dictionary<string, object>() { { "Token", t }, { "Valid", validUntil }, { "User", user } });
 
-                myReader.Close();
+                if (myConnection != null)
+                    myConnection.Close();
+                return JsonConvert.SerializeObject(new { success = ok, token = t, until = validUntil });
             }
-            catch (Exception e)
+            else
             {
-                return "Read Error: " + e.ToString();
+                if (myConnection != null)
+                    myConnection.Close();
+                return JsonConvert.SerializeObject(new { success = ok, problem = error });
             }
-
-            return JsonConvert.SerializeObject(new { success = ok });
         }
+
+
     }
 }
