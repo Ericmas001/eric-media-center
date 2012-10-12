e2fb4d858bb256322aad0271d1cf04fa1e523da5
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-10-12 14:09:44 +0000

    Deserializing TimeService & TvSchedule

--- trunk/EricMediaCenter/EMCAppTestPlugin/TestPanel.Designer.cs
+++ trunk/EricMediaCenter/EMCAppTestPlugin/TestPanel.Designer.cs
@@ -31,16 +31,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(474, 1);
+            this.button2.Location = new System.Drawing.Point(446, 1);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(103, 23);
             this.button2.TabIndex = 7;
             this.button2.Text = "TRY";
             this.button2.UseVisualStyleBackColor = true;
@@ -51,41 +49,23 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(11, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(121, 21);
+            this.listBox1.Size = new System.Drawing.Size(189, 21);
             this.listBox1.TabIndex = 12;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 3);
+            this.textBox1.Location = new System.Drawing.Point(206, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(303, 20);
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
             this.textBox1.TabIndex = 11;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(138, 6);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "ID:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(8, 38);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "RESULT:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(74, 35);
+            this.textBox3.Location = new System.Drawing.Point(11, 35);
+            this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(475, 20);
+            this.textBox3.Size = new System.Drawing.Size(538, 388);
             this.textBox3.TabIndex = 14;
             // 
             // TestPanel
@@ -93,13 +73,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
             this.Name = "TestPanel";
-            this.Size = new System.Drawing.Size(555, 64);
+            this.Size = new System.Drawing.Size(555, 435);
             this.Load += new System.EventHandler(this.TestPanel_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -111,8 +89,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
     }
 }
