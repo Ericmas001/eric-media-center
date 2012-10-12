c772540a070fdcca49809cc20fa3cd4e6147d35c
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-10-12 14:50:24 +0000

    WebServicePlugin

---
+++ trunk/EricMediaCenter/EMCMasterPluginLib/WebService/IEMCWebServicePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMCMasterPluginLib.WebService
+{
+    public interface IEMCWebServicePlugin : IEMCMasterPlugin
+    {
+        IWebService GetWebService();
+    }
+}
---
+++ trunk/EricMediaCenter/EMCMasterPluginLib/WebService/IWebService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EMCMasterPluginLib.WebService
+{
+    public interface IWebService
+    {
+        string Title { get; }
+        string BaseUrl { get; }
+        string[] Commands { get; }
+        object GetResult(string command, string args);
+    }
+}
