bc2f0f86affde587d3a140423315d208308aab55
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-10-18 11:25:46 +0000

    WatchSeriesWSPlugin!

---
+++ EricMediaCenter/EMCWatchSeriesWSPlugin/TvScheduleWSPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMCMasterPluginLib.WebService;
+
+namespace EMCTestWSPlugin
+{
+    public class TvScheduleWSPlugin : IEMCWebServicePlugin
+    {
+        #region IEMCWebServicePlugin Members
+
+        public IWebService GetWebService()
+        {
+            return new TvScheduleWebService();
+        }
+
+        #endregion
+
+        #region IEMCMasterPlugin Members
+
+        public string UniqueName
+        {
+            get { return "TvScheduleWSPlugin"; }
+        }
+
+        public string FullName
+        {
+            get { return "Webservice client for TvSchedule"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(0, 0, 1); }
+        }
+
+        public bool Load()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ EricMediaCenter/EMCWatchSeriesWSPlugin/WatchSeriesWSPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMCMasterPluginLib.WebService;
+
+namespace EMCTestWSPlugin
+{
+    public class WatchSeriesWSPlugin : IEMCWebServicePlugin
+    {
+        #region IEMCWebServicePlugin Members
+
+        public IWebService GetWebService()
+        {
+            return new WatchSeriesWebService();
+        }
+
+        #endregion
+
+        #region IEMCMasterPlugin Members
+
+        public string UniqueName
+        {
+            get { return "WatchSeriesWSPlugin"; }
+        }
+
+        public string FullName
+        {
+            get { return "Webservice client for WatchSeries"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(0, 0, 1); }
+        }
+
+        public bool Load()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
