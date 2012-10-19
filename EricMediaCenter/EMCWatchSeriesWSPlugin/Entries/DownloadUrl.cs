142d2d423748a87ddf3a45f7b7348ac34d1565aa
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-10-19 11:42:03 +0000

    EMCWatchSeriesWSPlugin is mostly done !

---
+++ EricMediaCenter/EMCWatchSeriesWSPlugin/Entries/DownloadUrl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace EMCWatchSeriesWSPlugin.Entries
+{
+    public class DownloadUrl
+    {
+        private string m_FullUrl;
+        private bool m_Supported;
+        private string m_WebSite;
+        private string m_Args;
+
+        public string FullUrl
+        {
+            get { return m_FullUrl; }
+            set { m_FullUrl = value; }
+        }
+
+        public bool Supported
+        {
+            get { return m_Supported; }
+            set { m_Supported = value; }
+        }
+
+        public string WebSite
+        {
+            get { return m_WebSite; }
+            set { m_WebSite = value; }
+        }
+
+        public string Args
+        {
+            get { return m_Args; }
+            set { m_Args = value; }
+        }
+
+        public DownloadUrl(string fullUrl, bool supported, string webSite, string args)
+        {
+            m_FullUrl = fullUrl;
+            m_Supported = supported;
+            m_WebSite = webSite;
+            m_Args = args;
+        }
+
+        public DownloadUrl(JObject r)
+        {
+            //{
+            //"url":"http://www.allmyvideos.net/ga0lxylkcuy7",
+            //"supported":false,
+            //"website":"allmyvideos.net",
+            //"args":null
+            //}
+
+            //{
+            //"url":"http://gorillavid.in/r7k9my4xu42c",
+            //"supported":true,
+            //"website":"gorillavid.in",
+            //"args":"r7k9my4xu42c"
+            //}
+            m_FullUrl = (string)r["url"];
+            m_Supported = (bool)r["supported"];
+            m_WebSite = (string)r["website"];
+            m_Args = (string)r["args"];
+        }
+        public override string ToString()
+        {
+            //TODO
+            return base.ToString();
+        }
+    }
+}
--- EricMediaCenter/EMCWatchSeriesWSPlugin/TvScheduleWebService.cs
+++ EricMediaCenter/EMCWatchSeriesWSPlugin/TvScheduleWebService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using EMCMasterPluginLib.WebService;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using EMCWatchSeriesWSPlugin.Entries;
 
 namespace EMCWatchSeriesWSPlugin
 {
@@ -44,7 +45,7 @@ namespace EMCWatchSeriesWSPlugin
 
         public object GetResult(string listChoice, string result)
         {
-            string newRes = result;
+            object newRes = result;
             switch (listChoice)
             {
                 case "AvailableSchedule": newRes = TvScheduleAvailableSchedule(result); break;
@@ -53,34 +54,22 @@ namespace EMCWatchSeriesWSPlugin
             return newRes;
         }
 
-        private string TvScheduleAvailableSchedule(string result)
+        private Dictionary<int,string> TvScheduleAvailableSchedule(string result)
         {
             //
             //[{"Key":"-1","Value":"Tuesday, October 16"},{"Key":"0","Value":"Wednesday, October 17"},{"Key":"1","Value":"Thursday, October 18"},{"Key":"2","Value":"Friday, October 19"},{"Key":"3","Value":"Saturday, October 20"},{"Key":"4","Value":"Sunday, October 21"},{"Key":"5","Value":"Monday, October 22"}]
-            string newRes = "";
-            JArray results = JsonConvert.DeserializeObject<dynamic>(result);
-            foreach (JObject r in results)
-            {
-                newRes += "(" + r["Key"].ToString() + ") " + r["Value"].ToString();
-                newRes += Environment.NewLine;
-            }
-            return newRes;
+            Dictionary<int, string> days = new Dictionary<int, string>();
+            foreach (JObject r in JsonConvert.DeserializeObject<dynamic>(result))
+                days.Add((int)r["Key"], (string)r["Value"]);
+            return days;
         }
 
-        private string TvScheduleGetSchedule(string result)
+        private List<EpisodeScheduleInfo> TvScheduleGetSchedule(string result)
         {
             //3
             //[{"Url":"http://watchseries.eu/serie/48_hours_mystery","ShowName":"48 Hours Mystery","ShowTitle":"The Preacher's Passion","Season":26,"Episode":4},{"Url":"#","ShowName":"ABC Saturday Night College Football","ShowTitle":"Florida State at Miami (FL)","Season":7,"Episode":6},{"Url":"http://watchseries.eu/serie/billy_the_exterminator","ShowName":"Billy the Exterminator","ShowTitle":"Fight or Flight","Season":5,"Episode":16},{"Url":"http://watchseries.eu/serie/billy_the_exterminator","ShowName":"Billy the Exterminator","ShowTitle":"Wasp Warfare","Season":5,"Episode":17},{"Url":"#","ShowName":"Bleach (US)","ShowTitle":"One Hit Kill, SuÃ¬-FÄng, Bankai!","Season":14,"Episode":11},{"Url":"#","ShowName":"CFB on FOX","ShowTitle":"Kansas State at West Virginia","Season":1,"Episode":8},{"Url":"#","ShowName":"Deadly Affairs","ShowTitle":"Killer Ambition","Season":1,"Episode":7},{"Url":"#","ShowName":"Doomsday Preppers Bugged Out","ShowTitle":"The End of the World as We Know It","Season":1,"Episode":2},{"Url":"http://watchseries.eu/serie/home_by_novogratz","ShowName":"Home by Novogratz","ShowTitle":"Williamsburg Couple's Modern Meets Natural Loft","Season":2,"Episode":12},{"Url":"#","ShowName":"Legends of","ShowTitle":"Alaska","Season":1,"Episode":99},{"Url":"http://watchseries.eu/serie/parking_wars","ShowName":"Parking Wars","ShowTitle":"Season 6, Episode 18","Season":6,"Episode":18},{"Url":"http://watchseries.eu/serie/parking_wars","ShowName":"Parking Wars","ShowTitle":"Season 6, Episode 19","Season":6,"Episode":19},{"Url":"http://watchseries.eu/serie/saturday_night_live","ShowName":"Saturday Night Live","ShowTitle":"Bruno Mars / Bruno Mars","Season":38,"Episode":5},{"Url":"http://watchseries.eu/serie/the_high_low_project","ShowName":"The High Low Project","ShowTitle":"Happy Medium Master Bedroom","Season":3,"Episode":3}]
-            string newRes = "";
             JArray results = JsonConvert.DeserializeObject<dynamic>(result);
-            foreach (JObject r in results)
-            {
-                newRes += r["ShowName"].ToString() + " " + r["Season"].ToString() + "x" + r["Episode"].ToString() + ": " + r["ShowTitle"].ToString();
-                if (r["Url"].ToString() != "#")
-                    newRes += " (" + r["Url"].ToString() + ")";
-                newRes += Environment.NewLine;
-            }
-            return newRes;
+            return EpisodeScheduleInfo.DeserializeEpisodes(results);
         }
 
 
