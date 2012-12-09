69c341199c1cfc4d8b94f7b15d14db664df5da6b
ericmas001@gmail.com <ericmas001@gmail.com@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2012-12-09 19:24:29 +0000

    AppTest is useless right now

--- trunk/EricMediaCenter/EMCAppTestPlugin/AppTestPlugin.cs
+++ trunk/EricMediaCenter/EMCAppTestPlugin/AppTestPlugin.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using EMCMasterPluginLib.Application;
-using System.Drawing;
-using System.Windows.Forms;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using EMCMasterPluginLib.Application;
+//using System.Drawing;
+//using System.Windows.Forms;
 
-namespace EMCAppTestPlugin
-{
-    public class AppTestPlugin : IEMCApplicationPlugin
-    {
-        #region IEMCApplicationPlugin Members
+//namespace EMCAppTestPlugin
+//{
+//    public class AppTestPlugin : IEMCApplicationPlugin
+//    {
+//        #region IEMCApplicationPlugin Members
 
-        public IApplication GetApplication()
-        {
-            return new MyTestApp();
-        }
+//        public IApplication GetApplication()
+//        {
+//            return new MyTestApp();
+//        }
 
-        #endregion
+//        #endregion
 
-        #region IEMCMasterPlugin Members
+//        #region IEMCMasterPlugin Members
 
-        public string UniqueName
-        {
-            get { return "appTestPlugin"; }
-        }
+//        public string UniqueName
+//        {
+//            get { return "appTestPlugin"; }
+//        }
 
-        public string FullName
-        {
-            get { return "Application de test"; }
-        }
+//        public string FullName
+//        {
+//            get { return "Application de test"; }
+//        }
 
-        public Version Version
-        {
-            get { return new Version(0, 0, 4); }
-        }
+//        public Version Version
+//        {
+//            get { return new Version(0, 0, 4); }
+//        }
 
-        public bool Load()
-        {
-            return true;
-        }
+//        public bool Load()
+//        {
+//            return true;
+//        }
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
