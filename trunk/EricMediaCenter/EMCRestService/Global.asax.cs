8bf7ed87a2fd6dfcd08652271403e33da1f9d015
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-03-17 13:31:14 +0000

    New "Bot" Service has "TvUpdate" working !

--- trunk/EricMediaCenter/EMCRestService/Global.asax.cs
+++ trunk/EricMediaCenter/EMCRestService/Global.asax.cs
@@ -15,19 +15,23 @@ namespace EMCRestService
 
         private void RegisterRoutes()
         {
-            // Edit the base address of Service1 by replacing the "Service1" string below
+            // Old Services => Soon to be deleted
             RouteTable.Routes.Add(new ServiceRoute("TimeService", new WebServiceHostFactory(), typeof(TimeService)));
             RouteTable.Routes.Add(new ServiceRoute("TvSchedule", new WebServiceHostFactory(), typeof(TvScheduleService)));
             RouteTable.Routes.Add(new ServiceRoute("VideoParsing", new WebServiceHostFactory(), typeof(VideoParsingService)));
             RouteTable.Routes.Add(new ServiceRoute("User", new WebServiceHostFactory(), typeof(UserService)));
-            RouteTable.Routes.Add(new ServiceRoute("Users", new WebServiceHostFactory(), typeof(UsersService)));
             RouteTable.Routes.Add(new ServiceRoute("WatchSeries", new WebServiceHostFactory(), typeof(WatchSeriesService)));
             RouteTable.Routes.Add(new ServiceRoute("TubePlus", new WebServiceHostFactory(), typeof(TubePlusService)));
             RouteTable.Routes.Add(new ServiceRoute("Automated", new WebServiceHostFactory(), typeof(AutomatedService)));
+
+
             RouteTable.Routes.Add(new ServiceRoute("EpGuide", new WebServiceHostFactory(), typeof(EpGuideService)));
             RouteTable.Routes.Add(new ServiceRoute("TvRage", new WebServiceHostFactory(), typeof(TvRageService)));
 
+            // New Services
             RouteTable.Routes.Add(new ServiceRoute("Tv", new WebServiceHostFactory(), typeof(TvService)));
+            RouteTable.Routes.Add(new ServiceRoute("Users", new WebServiceHostFactory(), typeof(UsersService)));
+            RouteTable.Routes.Add(new ServiceRoute("Bot", new WebServiceHostFactory(), typeof(BotService)));
         }
     }
 }
---
+++ trunk/EricMediaCenter/EMCRestService/Services/BotService.cs
@@ -0,0 +1,101 @@
+﻿using EMCRestService.TvWebsites.Entities;
+using EricUtility2011.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace EMCRestService.Services
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class BotService
+    {
+        private SqlServerConnector Connector = new SqlServerConnector("TURNSOL.arvixe.com", "emc2", "emc.webservice", "Emc42FTW");
+
+        [WebGet(UriTemplate = "TvUpdate")]
+        public string TvUpdate()
+        {
+
+            SqlConnection myConnection = Connector.GetConnection();
+            try
+            {
+                using (SqlCommand cmdAll = new SqlCommand("ericmas001.SPTvAllShows", myConnection))
+                {
+                    cmdAll.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader sdr = cmdAll.ExecuteReader();
+                    TvService tv = new TvService();
+                    List<object> changes = new List<object>();
+                    while (sdr.Read())
+                    {
+                        string website = (string)sdr["website"];
+                        string name = (string)sdr["showname"];
+                        string title = (string)sdr["showtitle"];
+                        int lastS = (int)sdr["lastSeason"];
+                        int lastE = (int)sdr["lastEpisode"];
+
+                        string res = tv.Show(website, name);
+                        if (res == null)
+                        {
+                            changes.Add(new { showname = name, website = website,  info = "Show not found" });
+                            continue;
+                        }
+
+                        TvShow show = JsonConvert.DeserializeObject<TvShow>(res);
+                        if (lastS == show.NoLastSeason && lastE == show.NoLastEpisode)
+                        {
+                            changes.Add(new { showname = name, website = website, info = "Already Up to Date" });
+                            continue;
+                        }
+                        try
+                        {
+                            using (SqlCommand cmdUpdade = new SqlCommand("ericmas001.SPTvLastEpisode", myConnection))
+                            {
+                                cmdUpdade.CommandType = CommandType.StoredProcedure;
+
+                                cmdUpdade.Parameters.Add("@website", SqlDbType.VarChar, 50);
+                                cmdUpdade.Parameters.Add("@name", SqlDbType.VarChar, 50);
+                                cmdUpdade.Parameters.Add("@lastSeason", SqlDbType.Int);
+                                cmdUpdade.Parameters.Add("@lastEpisode", SqlDbType.Int);
+                                cmdUpdade.Parameters.Add("@ok", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                                cmdUpdade.Parameters.Add("@info", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+
+                                cmdUpdade.Parameters["@website"].Value = website;
+                                cmdUpdade.Parameters["@name"].Value = name;
+                                cmdUpdade.Parameters["@lastSeason"].Value = show.NoLastSeason;
+                                cmdUpdade.Parameters["@lastEpisode"].Value = show.NoLastEpisode;
+
+                                cmdUpdade.ExecuteNonQuery();
+
+                                changes.Add(new { showname = name, website = website, info = (String)cmdUpdade.Parameters["@info"].Value });
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            if (myConnection != null)
+                                myConnection.Close();
+                            return JsonConvert.SerializeObject(new { success = false, problem = e.ToString() });
+                        }
+                    }
+                    sdr.Close();
+                    if (myConnection != null)
+                        myConnection.Close();
+                    return JsonConvert.SerializeObject(new { success = true, log = changes });
+                }
+            }
+            catch (Exception e)
+            {
+                if (myConnection != null)
+                    myConnection.Close();
+                return JsonConvert.SerializeObject(new { success = false, problem = e.ToString() });
+            }
+        }
+    }
+}
