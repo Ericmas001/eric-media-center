448cf1f44a4c1c02b2c1d1c2e58459ac88b84090
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-03-17 01:36:04 +0000

    Building the new TUsers in the database with it's stored procedures

---
+++ trunk/EricMediaCenter/EricMediaCenter/Database/TUser.sql
@@ -0,0 +1,30 @@
+﻿USE [emc2]
+GO
+
+/****** Object:  Table [ericmas001].[TUser]    Script Date: 2013-03-16 21:09:16 ******/
+DROP TABLE [ericmas001].[TUser]
+GO
+
+/****** Object:  Table [ericmas001].[TUser]    Script Date: 2013-03-16 21:09:16 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [ericmas001].[TUser](
+	[idUser] [int] IDENTITY(1,1) NOT NULL,
+	[username] [nvarchar](50) NOT NULL,
+	[password] [nvarchar](32) NOT NULL,
+	[email] [nvarchar](100) NULL,
+	[sessionToken] [nvarchar](32) NULL,
+	[validUntil] [datetime] NULL,
+ CONSTRAINT [PK_TUser] PRIMARY KEY CLUSTERED 
+(
+	[idUser] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
