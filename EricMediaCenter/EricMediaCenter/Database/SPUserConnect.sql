52192f16896befe8605aaf6eded0d415b38427a2
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-03-17 02:21:37 +0000

    Stored procedure for Connect, Register and RefreshToken are done

--- EricMediaCenter/EricMediaCenter/Database/SPUserConnect.sql
+++ EricMediaCenter/EricMediaCenter/Database/SPUserConnect.sql
@@ -13,40 +13,47 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-IF OBJECT_ID('SPUserRegister') IS NOT NULL
-DROP PROC SPUserRegister
+IF OBJECT_ID('SPUserConnect') IS NOT NULL
+DROP PROC SPUserConnect
 GO
 -- =============================================
 -- Author:		ericmas001
 -- Create date: 2013-03-16
 -- Description:	Register a user
 -- =============================================
-CREATE PROCEDURE SPUserRegister 
+CREATE PROCEDURE SPUserConnect 
 	-- Add the parameters for the stored procedure here
 	@username NVARCHAR(50), 
-	@password NVARCHAR(50), 
-	@email NVARCHAR(100),
-	@ok BIT OUT,
-	@info NVARCHAR(100) OUT
+	@password NVARCHAR(50),
+	@ok BIT = 0 OUT,
+	@info NVARCHAR(100) OUT,
+	@validUntil DATETIME OUT
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SET @ok = 1
-	SELECT @ok = 0 from [ericmas001].[TUser] where username = @username
+	DECLARE @hashed NVARCHAR(32)
+	SELECT @hashed = CONVERT(NVARCHAR(32),HashBytes('MD5', @password),2)
+
+	DECLARE @idUser INT
+	SET @ok = 0
+	SELECT @ok = 1, @idUser = idUser from [ericmas001].[TUser] where username = @username AND password = @hashed
 
 	IF @ok = 1
 	BEGIN
-		DECLARE @hashed NVARCHAR(32)
-		SELECT @hashed = CONVERT(NVARCHAR(32),HashBytes('MD5', @password),2)
-
-		INSERT INTO [ericmas001].[TUser] (username, password, email) Values(@username, @hashed, @email)
+		DECLARE @key NVARCHAR(100)
+		SELECT @key = @username + '_' + CONVERT(NVARCHAR(100),NEWID())
+		DECLARE @token NVARCHAR(32)
+		SELECT @token = CONVERT(NVARCHAR(32),HashBytes('MD5',@key ),2)
+		UPDATE [ericmas001].[TUser] SET sessionToken = @token WHERE idUser = @idUser
+		EXEC [ericmas001].[SPUserRefresh] @idUser, @validUntil output
+		SELECT @info = @token
 	END
 	ELSE
 	BEGIN
-		SET @info = 'The username already exists'
+		SET @info = 'Incorrect user or password'
 	END
 END
 GO
