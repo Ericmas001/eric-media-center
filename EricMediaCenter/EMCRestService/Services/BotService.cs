76bc05bdd2edafb70f9bdf3c30a3616bf16b9416
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-03-17 14:42:22 +0000

    Using SP functions of SqlServerConnector

--- EricMediaCenter/EMCRestService/Services/BotService.cs
+++ EricMediaCenter/EMCRestService/Services/BotService.cs
@@ -22,73 +22,51 @@ namespace EMCRestService.Services
         [WebGet(UriTemplate = "TvUpdate")]
         public string TvUpdate()
         {
-
             SqlConnection myConnection = Connector.GetConnection();
             try
             {
-                using (SqlCommand cmdAll = new SqlCommand("ericmas001.SPTvAllShows", myConnection))
+                SPResult rAll = Connector.SelectRowsSP(myConnection, "ericmas001.SPTvAllShows");
+                TvService tv = new TvService();
+                List<object> changes = new List<object>();
+                foreach (Dictionary<string, object> r in rAll.QueryResults)
                 {
-                    cmdAll.CommandType = CommandType.StoredProcedure;
+                    string website = (string)r["website"];
+                    string name = (string)r["showname"];
+                    string title = (string)r["showtitle"];
+                    int lastS = (int)r["lastSeason"];
+                    int lastE = (int)r["lastEpisode"];
 
-                    SqlDataReader sdr = cmdAll.ExecuteReader();
-                    TvService tv = new TvService();
-                    List<object> changes = new List<object>();
-                    while (sdr.Read())
+                    string res = tv.Show(website, name);
+                    if (res == null)
                     {
-                        string website = (string)sdr["website"];
-                        string name = (string)sdr["showname"];
-                        string title = (string)sdr["showtitle"];
-                        int lastS = (int)sdr["lastSeason"];
-                        int lastE = (int)sdr["lastEpisode"];
-
-                        string res = tv.Show(website, name);
-                        if (res == null)
-                        {
-                            changes.Add(new { showname = name, website = website,  info = "Show not found" });
-                            continue;
-                        }
-
-                        TvShow show = JsonConvert.DeserializeObject<TvShow>(res);
-                        if (lastS == show.NoLastSeason && lastE == show.NoLastEpisode)
-                        {
-                            changes.Add(new { showname = name, website = website, info = "Already Up to Date" });
-                            continue;
-                        }
-                        try
-                        {
-                            using (SqlCommand cmdUpdade = new SqlCommand("ericmas001.SPTvLastEpisode", myConnection))
-                            {
-                                cmdUpdade.CommandType = CommandType.StoredProcedure;
-
-                                cmdUpdade.Parameters.Add("@website", SqlDbType.VarChar, 50);
-                                cmdUpdade.Parameters.Add("@name", SqlDbType.VarChar, 50);
-                                cmdUpdade.Parameters.Add("@lastSeason", SqlDbType.Int);
-                                cmdUpdade.Parameters.Add("@lastEpisode", SqlDbType.Int);
-                                cmdUpdade.Parameters.Add("@ok", SqlDbType.Bit).Direction = ParameterDirection.Output;
-                                cmdUpdade.Parameters.Add("@info", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
-
-                                cmdUpdade.Parameters["@website"].Value = website;
-                                cmdUpdade.Parameters["@name"].Value = name;
-                                cmdUpdade.Parameters["@lastSeason"].Value = show.NoLastSeason;
-                                cmdUpdade.Parameters["@lastEpisode"].Value = show.NoLastEpisode;
-
-                                cmdUpdade.ExecuteNonQuery();
-
-                                changes.Add(new { showname = name, website = website, info = (String)cmdUpdade.Parameters["@info"].Value });
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            if (myConnection != null)
-                                myConnection.Close();
-                            return JsonConvert.SerializeObject(new { success = false, problem = e.ToString() });
-                        }
+                        changes.Add(new { showname = name, website = website, info = "Show not found" });
+                        continue;
                     }
-                    sdr.Close();
-                    if (myConnection != null)
-                        myConnection.Close();
-                    return JsonConvert.SerializeObject(new { success = true, log = changes });
+
+                    TvShow show = JsonConvert.DeserializeObject<TvShow>(res);
+                    if (lastS == show.NoLastSeason && lastE == show.NoLastEpisode)
+                    {
+                        changes.Add(new { showname = name, website = website, info = "Already Up to Date" });
+                        continue;
+                    }
+
+                    List<SPParam> parms = new List<SPParam>
+                    {
+                        new SPParam(new SqlParameter("@website", SqlDbType.VarChar, 50),website),
+                        new SPParam(new SqlParameter("@name", SqlDbType.VarChar, 50),name),
+                        new SPParam(new SqlParameter("@lastSeason", SqlDbType.Int),show.NoLastSeason),
+                        new SPParam(new SqlParameter("@lastEpisode", SqlDbType.Int),show.NoLastEpisode),
+                        new SPParam(new SqlParameter("@ok", SqlDbType.Bit),ParamDir.Output),
+                        new SPParam(new SqlParameter("@info", SqlDbType.VarChar, 100),ParamDir.Output),
+                    };
+
+                    Dictionary<string,object> p = Connector.ExecuteSP(myConnection, "ericmas001.SPTvLastEpisode", parms).Parameters;
+
+                    changes.Add(new { showname = name, website = website, info = (String)p["@info"] });
                 }
+                if (myConnection != null)
+                    myConnection.Close();
+                return JsonConvert.SerializeObject(new { success = true, log = changes });
             }
             catch (Exception e)
             {
