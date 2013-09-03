a752a04de9a996289d9f4318d8684df6db1f6c11
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-09-03 23:43:33 +0000

    divxstage.eu, movshare.net, novamov.com, nowvideo.eu, videoweed.es all supported !!

---
+++ trunk/EricMediaCenter/EMCTv/VideoParser/NovaMovParser.cs
@@ -0,0 +1,18 @@
+﻿using EricUtility;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EMCTv.VideoParser
+{
+
+    public class NovaMovParser : VideoWeedParser
+    {
+        public override async Task<string> GetDownloadUrlAsync(string url, System.Net.CookieContainer cookies)
+        {
+            string res = await base.GetDownloadUrlAsync(url, cookies);
+            return Uri.UnescapeDataString(res);
+        }
+    }
+}
