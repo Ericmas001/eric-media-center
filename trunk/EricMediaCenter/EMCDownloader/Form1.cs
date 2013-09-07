02b1680d6a32ad3b1b7043d1570444957514d018
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-09-07 12:36:03 +0000

    First Glimpse of EMCDownloader !

---
+++ trunk/EricMediaCenter/EMCDownloader/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EMCDownloader
+{
+    public partial class Form1 : Form
+    {
+        public string[] Args;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // The single-instance code is going to save the command line 
+            // arguments in this member variable before opening the first instance
+            // of the app.
+            if (this.Args != null)
+            {
+                ProcessParameters(null, this.Args);
+                this.Args = null;
+            }
+        }
+
+        public delegate void ProcessParametersDelegate(object sender, string[] args);
+        public void ProcessParameters(object sender, string[] args)
+        {
+            // The form has loaded, and initialization will have been be done.
+
+            // Add the command-line arguments to our textbox, just to confirm that
+            // it reached here.
+            if (args != null && args.Length != 0)
+                listBox1.Items.Add(String.Format("{0} {1}", DateTime.Now.ToString("mm:ss.ff"), String.Join(" ", args)));
+        }
+    }
+}
--- trunk/EricMediaCenter/EMCTv/Windows.Forms/OpenURLForm.cs
+++ trunk/EricMediaCenter/EMCTv/Windows.Forms/OpenURLForm.cs
@@ -15,9 +15,13 @@ namespace EMCTv.Windows.Forms
     public partial class OpenURLForm : Form
     {
         private StreamingInfo m_StreamingInfo = null;
-        public OpenURLForm(StreamingInfo si)
+        private TvShow m_Show = null;
+        private Episode m_Episode = null;
+        public OpenURLForm(StreamingInfo si, TvShow show, Episode ep)
         {
             m_StreamingInfo = si;
+            m_Show = show;
+            m_Episode = ep;
             InitializeComponent();
         }
 
@@ -37,6 +41,20 @@ namespace EMCTv.Windows.Forms
         {
             Process.Start(m_StreamingInfo.StreamingURL);
             Close();
+        }
+
+        private void btnDwldEMCD_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(Properties.Settings.Default.emcd_path) && !String.IsNullOrWhiteSpace(Properties.Settings.Default.emcd_output))
+            {
+                string src = m_StreamingInfo.DownloadURL;
+                string dest = Properties.Settings.Default.emcd_output + "/" + m_Show.Title + " S" + m_Episode.NoSeason + "E" + m_Episode.NoEpisode + ".flv";
+                // TODO: chg name if already exist (1) (2) ...
+                // TODO: exstension !!
+                // TODO: padding S3E5 => S03E05
+                string args = "\"" + src + "\" \"" + dest + "\"";
+                Process.Start(Properties.Settings.Default.emcd_path, args);
+            }
         }
     }
 }
