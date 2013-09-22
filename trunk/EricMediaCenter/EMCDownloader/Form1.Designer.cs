ac381492833b48a9168b93267168254f75aaa2d2
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-09-22 16:00:05 +0000

    Pimp the download manager

--- trunk/EricMediaCenter/EMCDownloader/Form1.Designer.cs
+++ trunk/EricMediaCenter/EMCDownloader/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.downloadList1 = new EricUtility.Windows.Forms.Downloader.DownloadList();
             this.SuspendLayout();
             // 
@@ -46,8 +47,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(966, 442);
             this.Controls.Add(this.downloadList1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EMC Downloader";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
