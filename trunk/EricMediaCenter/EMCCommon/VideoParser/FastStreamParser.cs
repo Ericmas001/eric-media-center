93c469057d76f1aaff45b32d533a52b7b88a8ac0
ericmas001 <ericmas001@177f3f7c-f70d-a1c7-4a88-a02ce38e818b>
2013-10-27 17:07:09 +0000

    Adding FastStream & FileNuke on board

--- trunk/EricMediaCenter/EMCCommon/VideoParser/FastStreamParser.cs
+++ trunk/EricMediaCenter/EMCCommon/VideoParser/FastStreamParser.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Threading;
 
 namespace EMCCommon.VideoParser
 {
-    public class GorillaVidParser : Abstract2StepsParser
+    public class FastStreamParser : Abstract2StepsParser
     {
         public override string WaitingScreenSentence
         {
-            get { return "Please wait while we verify your request"; }
+            get { return "<input type=\"submit\" name=\"imhuman\" value=\"Continue to video\" id=\"btn_download\">"; }
         }
 
         public override async Task<IEnumerable<KeyValuePair<string, string>>> GetParameters(string res)
         {
+            string hash = res.Extract("<input type=\"hidden\" name=\"hash\" value=\"", "\">");
+            Thread.Sleep(2000);
             return (await base.GetParameters(res)).Union(new[]
                 {
-                    new KeyValuePair<string, string>("channel", ""),
-                    new KeyValuePair<string, string>("method_free", "tel%3Fchargement+libre+"),
+                    new KeyValuePair<string, string>("hash", hash),
                 });
         }
     }
--- trunk/EricMediaCenter/EMCCommon/VideoParser/FileNukeParser.cs
+++ trunk/EricMediaCenter/EMCCommon/VideoParser/FileNukeParser.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace EMCCommon.VideoParser
 {
-    public class GorillaVidParser : Abstract2StepsParser
+    public class FileNukeParser : Abstract2StepsParser
     {
         public override string WaitingScreenSentence
         {
-            get { return "Please wait while we verify your request"; }
+            get { return "Choose how to download"; }
         }
 
         public override async Task<IEnumerable<KeyValuePair<string, string>>> GetParameters(string res)
         {
             return (await base.GetParameters(res)).Union(new[]
                 {
-                    new KeyValuePair<string, string>("channel", ""),
-                    new KeyValuePair<string, string>("method_free", "tel%3Fchargement+libre+"),
+                    new KeyValuePair<string, string>("method_free", "Free"),
                 });
+        }
+
+        public override string ExtractFile(string res)
+        {
+            return VideoParsingFactory.ExecuteScript(res.Extract("<div id=\"player_code\">", "</div>")).Extract("\"src=\"","\"");
         }
     }
 }
